5062af98b3a91f28bc4edde1f7bfed04a5a6cc69
Harshith R <harshith.raju0710@gmail.com>
2024-09-19 13:46:28 +0530

    set up initial files

---
+++ src/app/store.js
@@ -0,0 +1,6 @@
+// this is a very important file whenever you are setting up using redux
+// this is a single source of truth so keep only one store.js fileand keep all the slices here
+
+import {configureStore} from '@reduxjs/toolkit'
+
+export const store = configureStore()
